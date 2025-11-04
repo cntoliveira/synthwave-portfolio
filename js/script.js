eea4f59b5ec3fdfa7fb4d809357293399f174c64
cntoliveira <canotdev@gmail.com>
2025-11-04 01:54:09 -0300

    Add custom neon animated cursos + dot w outline

--- js/script.js
+++ js/script.js
@@ -37,3 +37,49 @@ function updateIcons(){
 }
 
 themeToggle.addEventListener('click', toggleTheme);
+
+/*add cursor pointer logic*/
+
+const cursorDot = document.querySelector('.cursor-dot');
+const cursorOutline = document.querySelector('.cursor-outline');
+
+let mouseX = 0;
+let mouseY = 0;
+let outlineX = 0;
+let outlineY = 0;
+
+window.addEventListener('mousemove', (e) => {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+
+    cursorDot.style.left = mouseX + 'px';
+    cursorDot.style.top = mouseY + 'px';
+});
+
+function animateOutline() {
+    const speed = 0.1;
+
+    outlineX += (mouseX - outlineX) * speed;
+    outlineY += (mouseY - outlineY) * speed;
+
+    cursorOutline.style.left = outlineX + 'px';
+    cursorOutline.style.top = outlineY + 'px';
+
+    requestAnimationFrame(animateOutline);
+}
+
+animateOutline();
+
+const hoverElements = document.querySelectorAll('a, button');
+
+hoverElements.forEach(el => {
+    el.addEventListener('mouseenter', () => {
+        cursorDot.classList.add('hover');
+        cursorOutline.classList.add('hover');
+    });
+
+     el.addEventListener('mouseleave', () => {
+        cursorDot.classList.remove('hover');
+        cursorOutline.classList.remove('hover');
+    });
+})
